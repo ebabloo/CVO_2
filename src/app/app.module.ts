fa25326acca5ca23ef6961bc2220e8aee53dc9cd
ravindra <bbss@overleaptech.com>
2021-02-13 00:51:40 +0530

    personal-info form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { ManagecredentialsModule } from './components/managecredentials/managecredentials.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ManagecredentialsModule } from "./components/managecredentials/managecredentials.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgCircleProgressModule } from 'ng-circle-progress';
-import { AdmindashboardComponent } from './components/admindashboard/admindashboard.component';
-import { PhysiciandashboardComponent } from './components/physiciandashboard/physiciandashboard.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { EmployeesComponent } from './components/employees/employees.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-
-import { PersonalInformationComponent } from './components/personal-information/personal-information.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgCircleProgressModule } from "ng-circle-progress";
 
 
-import { LandingComponent } from './layouts/landing/landing.component';
-import { FullComponent } from './layouts/full/full.component';
-import { CustomMaterialModule } from './material';
+import { AdmindashboardComponent } from "./components/admindashboard/admindashboard.component";
+import { PhysiciandashboardComponent } from "./components/physiciandashboard/physiciandashboard.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { EmployeesComponent } from "./components/employees/employees.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+
+import { PersonalInformationComponent } from "./components/personal-information/personal-information.component";
+
+import { LandingComponent } from "./layouts/landing/landing.component";
+import { FullComponent } from "./layouts/full/full.component";
+import { CustomMaterialModule } from "./material";
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,8 +31,8 @@ import { CustomMaterialModule } from './material';
     SignupComponent,
     PersonalInformationComponent,
     LandingComponent,
-    FullComponent
-  ],  
+    FullComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -46,10 +47,9 @@ import { CustomMaterialModule } from './material';
       outerStrokeColor: "#78C000",
       innerStrokeColor: "#C7E596",
       animationDuration: 300,
-
-    })
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/managecredentials/managecredentials.component.ts
+++ src/app/components/managecredentials/managecredentials.component.ts
@@ -1,18 +1,16 @@
-import { Router } from '@angular/router';
-import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { MediaMatcher } from "@angular/cdk/layout";
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-managecredentials',
-  templateUrl: './managecredentials.component.html',
-  styleUrls: ['./managecredentials.component.css']
+  selector: "app-managecredentials",
+  templateUrl: "./managecredentials.component.html",
+  styleUrls: ["./managecredentials.component.css"],
 })
 export class ManagecredentialsComponent implements OnInit, OnDestroy {
+  selectedPath: string = "./personalInfo";
 
-
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   mobileQuery: MediaQueryList;
 
@@ -20,64 +18,68 @@ export class ManagecredentialsComponent implements OnInit, OnDestroy {
     {
       displayName: "Personal Information",
       path: "./personalInfo",
-      icon: ""
+      icon: "",
     },
     {
-      displayName: 'Work Experience',
+      displayName: "Work Experience",
       path: "./workexp",
-      icon: ''
+      icon: "",
     },
     {
-      displayName: 'Medical Mal Practice',
-      path: './medicalmalpractice',
-      icon: ''
+      displayName: "Medical Mal Practice",
+      path: "./medicalmalpractice",
+      icon: "",
     },
     {
-      displayName: 'Peer References',
-      path: './peerref',
-      icon: ''
+      displayName: "Peer References",
+      path: "./peerref",
+      icon: "",
     },
     {
-      displayName: 'Licensure',
-      path: './licensure',
-      icon: ''
+      displayName: "Licensure",
+      path: "./licensure",
+      icon: "",
     },
     {
-      displayName: 'Payor\'s Information',
-      path: './payorinfo',
-      icon: ''
+      displayName: "Payor's Information",
+      path: "./payorinfo",
+      icon: "",
     },
     {
-      displayName: 'CME',
-      path: './cme',
-      icon: ''
+      displayName: "CME",
+      path: "./cme",
+      icon: "",
     },
     {
-      displayName: 'Documents',
-      path: './documents',
-      icon: ''
+      displayName: "Documents",
+      path: "./documents",
+      icon: "",
     },
     {
-      displayName: 'Summary',
-      path: './summary',
-      icon: ''
-    }
-  ]
+      displayName: "Summary",
+      path: "./summary",
+      icon: "",
+    },
+  ];
 
-  fillerContent = Array.from({ length: 50 }, () =>
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+  fillerContent = Array.from(
+    { length: 50 },
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  );
 
   private _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher,
-    private router: Router) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    private router: Router
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
@@ -86,10 +88,16 @@ export class ManagecredentialsComponent implements OnInit, OnDestroy {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some((h) =>
+    h.test(window.location.host)
+  );
 
   onClick(itemData: any) {
-    console.log(itemData)
-    this.router.navigate(['./personalInfo'])
+    console.log(itemData);
+    this.router.navigate(["./personalInfo"]);
+  }
+
+  onItemClick(path: string) {
+    this.selectedPath = path;
   }
 }
