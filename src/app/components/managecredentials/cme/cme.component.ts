2d1490482d6e101d5df17297067b83d8ee3c5e4f
ravindra <bbss@overleaptech.com>
2021-02-19 00:10:04 +0530

    CME  Form

--- src/app/components/managecredentials/cme/cme.component.ts
+++ src/app/components/managecredentials/cme/cme.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface DropdownValues {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-cme',
   templateUrl: './cme.component.html',
   styleUrls: ['./cme.component.css']
 })
 export class CmeComponent implements OnInit {
+
+  creditType: DropdownValues[] = [
+    {value: 'installment', viewValue: 'Installment'},
+    {value: 'revolving', viewValue: 'Revolving'},
+    {value: 'open', viewValue: 'Open'}
+  ];
 
   constructor() { }
 
